a88d0333dc94c660c841d25267a152a1c81e5e81
Ivan Lukonin <lukoninis@yandex.ru>
2021-09-23 00:25:34 +0300

    more tests for sort_students

--- sort-students/test.cpp
+++ sort-students/test.cpp
@@ -3,6 +3,27 @@
 
 #include <iostream>
 
+bool operator==(const Student& lhs, const Student& rhs) {
+    return lhs.name == rhs.name && lhs.surname == rhs.surname && lhs.year == rhs.year &&
+           lhs.month == rhs.month && lhs.day == rhs.day;
+}
+
+std::ostream& operator<<(std::ostream& s, const Student& lhs) {
+    s << "(" << lhs.name << "," << lhs.surname << "," << lhs.day << "," << lhs.month << ","
+      << lhs.year << ")";
+    return s;
+}
+
+Student MakeStudent(std::string_view name, std::string_view surname, int year, int month, int day) {
+    Student result;
+    result.name = name;
+    result.surname = surname;
+    result.year = year;
+    result.month = month;
+    result.day = day;
+    return result;
+}
+
 TEST_CASE("By date test") {
     Student aa000("a", "a", 0, 0, 0),
         ab000("a", "b", 0, 0, 0),
@@ -20,4 +41,42 @@ TEST_CASE("By date test") {
     REQUIRE(st[3] == bb100);
     REQUIRE(st[4] == bb101);
     REQUIRE(st[5] == bb110);
+}
+
+TEST_CASE("by_date") {
+    std::vector<Student> test;
+    std::vector<Student> expected;
+    test.push_back(MakeStudent("c", "b", 1999, 9, 19));
+    test.push_back(MakeStudent("a", "b", 2000, 8, 12));
+    test.push_back(MakeStudent("b", "a", 2000, 11, 12));
+    test.push_back(MakeStudent("a", "c", 1998, 10, 10));
+    test.push_back(MakeStudent("a", "b", 2000, 11, 12));
+
+    expected.push_back(MakeStudent("a", "c", 1998, 10, 10));
+    expected.push_back(MakeStudent("c", "b", 1999, 9, 19));
+    expected.push_back(MakeStudent("a", "b", 2000, 8, 12));
+    expected.push_back(MakeStudent("b", "a", 2000, 11, 12));
+    expected.push_back(MakeStudent("a", "b", 2000, 11, 12));
+
+    SortStudents(&test, SortType::kByDate);
+    REQUIRE(expected == test);
+}
+
+TEST_CASE("by_name") {
+    std::vector<Student> test;
+    std::vector<Student> expected;
+    test.push_back(MakeStudent("c", "b", 1999, 9, 19));
+    test.push_back(MakeStudent("a", "b", 2000, 8, 12));
+    test.push_back(MakeStudent("b", "a", 2000, 11, 12));
+    test.push_back(MakeStudent("a", "c", 1998, 10, 10));
+    test.push_back(MakeStudent("a", "b", 2000, 11, 12));
+
+    expected.push_back(MakeStudent("b", "a", 2000, 11, 12));
+    expected.push_back(MakeStudent("a", "b", 2000, 8, 12));
+    expected.push_back(MakeStudent("a", "b", 2000, 11, 12));
+    expected.push_back(MakeStudent("c", "b", 1999, 9, 19));
+    expected.push_back(MakeStudent("a", "c", 1998, 10, 10));
+
+    SortStudents(&test, SortType::kByName);
+    REQUIRE(expected == test);
 }
